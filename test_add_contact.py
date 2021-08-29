a0d40fd18de17c0ac565cf9e6598663fbd61a661
Amal Tassimov <jubasto2000@gmail.com>
2021-08-29 16:46:08 +0300

    deleted unnecessary stuff

--- test_add_contact.py
+++ test_add_contact.py
@@ -2,17 +2,12 @@
 import unittest
 
 from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
 
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
 
     def test_untitled_test_case(self):
         driver = self.driver
@@ -45,36 +40,10 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_link_text("home page").click()
         driver.find_element_by_link_text("Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
 
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
 
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 
-if __name__ == "__main__":
-    unittest.main()
+
