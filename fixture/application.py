9911c51cbe8a8e7f5cffb27e4fdfc0598df2260b
Amal Tassimov <jubasto2000@gmail.com>
2021-08-31 00:22:41 +0300

    Changed parameter name to lowercase

--- fixture/application.py
+++ fixture/application.py
@@ -11,16 +11,16 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
-    def create_new_contact(self, Contact):
+    def create_new_contact(self, contact):
         self.driver.find_element_by_link_text("add new").click()
-        self.driver.find_element_by_name("firstname").send_keys(Contact.firstname)
-        self.driver.find_element_by_name("middlename").send_keys(Contact.middlename)
-        self.driver.find_element_by_name("lastname").send_keys(Contact.lastname)
-        self.driver.find_element_by_name("nickname").send_keys(Contact.nickname)
-        self.driver.find_element_by_name("title").send_keys(Contact.title)
-        self.driver.find_element_by_name("company").send_keys(Contact.company)
-        self.driver.find_element_by_name("address").send_keys(Contact.address)
-        self.driver.find_element_by_name("email").send_keys(Contact.email)
+        self.driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        self.driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        self.driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        self.driver.find_element_by_name("nickname").send_keys(contact.nickname)
+        self.driver.find_element_by_name("title").send_keys(contact.title)
+        self.driver.find_element_by_name("company").send_keys(contact.company)
+        self.driver.find_element_by_name("address").send_keys(contact.address)
+        self.driver.find_element_by_name("email").send_keys(contact.email)
         self.driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.driver.find_element_by_link_text("home page").click()
 
