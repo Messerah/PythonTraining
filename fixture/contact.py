43cbfd51bb8d7c9b8019f5ac4aa3cbf6fd07b171
Amal Tassimov <jubasto2000@gmail.com>
2021-09-04 12:52:16 +0300

    added new test - update contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -22,3 +22,16 @@ class ContactHelper:
         driver.find_element_by_xpath("//input[@value='Delete']").click()
         driver.switch_to_alert().accept()
 
+    def update(self, contact):
+        driver = self.app.driver
+        driver.find_element_by_css_selector("img[alt='Edit']").click()
+        driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
+        driver.find_element_by_name("title").send_keys(contact.title)
+        driver.find_element_by_name("company").send_keys(contact.company)
+        driver.find_element_by_name("address").send_keys(contact.address)
+        driver.find_element_by_name("email").send_keys(contact.email)
+        driver.find_element_by_name("update").click()
+        driver.find_element_by_link_text("home page").click()
---
+++ tests/test_update_contact.py
@@ -0,0 +1,9 @@
+from model.Contact import Contact
+
+
+def test_update_contact(app):
+    app.session.login()
+    app.contact.update(
+        Contact(firstname="Updated", middlename="Updated", lastname="Updated", nickname="Updated", title="Updated",
+                company="Updated", address="Updated", email="trolo@yandex.ru"))
+    app.session.logout()
