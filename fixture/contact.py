f8ccc6ad29d05429f5716565e75f457032d5cb01
Amal Tassimov <jubasto2000@gmail.com>
2021-10-05 00:48:55 +0300

    Homework 11 is done

--- fixture/contact.py
+++ fixture/contact.py
@@ -11,16 +11,26 @@ class ContactHelper:
         self.fill_in_form(contact, driver)
         driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_homepage(driver)
+        self.contact_cache = None
 
     def fill_in_form(self, contact, driver):
+        driver.find_element_by_name("firstname").clear()
         driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        driver.find_element_by_name("middlename").clear()
         driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        driver.find_element_by_name("lastname").clear()
         driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        driver.find_element_by_name("nickname").clear()
         driver.find_element_by_name("nickname").send_keys(contact.nickname)
+        driver.find_element_by_name("title").clear()
         driver.find_element_by_name("title").send_keys(contact.title)
+        driver.find_element_by_name("company").clear()
         driver.find_element_by_name("company").send_keys(contact.company)
+        driver.find_element_by_name("address").clear()
         driver.find_element_by_name("address").send_keys(contact.address)
+        driver.find_element_by_name("email").clear()
         driver.find_element_by_name("email").send_keys(contact.email)
+
 
     def delete(self):
         driver = self.app.driver
@@ -28,6 +38,7 @@ class ContactHelper:
         driver.find_element_by_name("selected[]").click()
         driver.find_element_by_xpath("//input[@value='Delete']").click()
         driver.switch_to_alert().accept()
+        self.contact_cache = None
 
     def open_homepage(self, driver):
         if not (driver.current_url.endswith("addressbook/") and len(driver.find_elements_by_name("Delete")) > 0):
@@ -40,6 +51,7 @@ class ContactHelper:
         self.fill_in_form(contact, driver)
         driver.find_element_by_name("update").click()
         self.return_to_homepage(driver)
+        self.contact_cache = None
 
     def return_to_homepage(self, driver):
         driver.find_element_by_link_text("home page").click()
--- tests/test_update_contact.py
+++ tests/test_update_contact.py
@@ -7,12 +7,12 @@ def test_update_first_contact(app):
             Contact(firstname="Trolo", middlename="trololoev", lastname="Trolevich", nickname="Trol", title="God",
                     company="JSC", address="TroloLand", email="trolo@yandex.ru"))
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Updated", middlename="Updated", lastname="Updated", nickname="Updated",
-                      title="Updated",
-                      company="Updated", address="Updated", email="trolo@yandex.ru")
+    contact = Contact(firstname="Trolo", middlename="trololoev", lastname="Trolevich", nickname="Trol", title="God",
+                      company="JSC", address="TroloLand", email="trolo@yandex.ru")
+
     contact.id = old_contacts[0].id
     app.contact.update(contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[1] = contact
+    old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
